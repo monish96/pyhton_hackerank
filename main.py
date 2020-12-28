120d22c45dc35ea3ff0069502ea7db9d7b90d38f
monish n <knockme96@gmail.com>
2020-12-28 18:51:10 +0530

    Day 01 # List comprehension

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Permutation example
+author - Monish N
+"""
+
+
+def return_permutation_result(x, y, z, n):
+    eliminated_list = [[a, b, c] for a in range(x + 1) for b in range(y + 1) for c in range(z + 1) if a + b + c != n]
+    return eliminated_list
+
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    print(return_permutation_result(x, y, z, n))
