86a61385c7e3016fabb8e6278ac6ea6dfe90f36b
monish n <knockme96@gmail.com>
2020-12-28 19:10:04 +0530

    Day 02 # Runner up challenge

---
+++ runnerUpScore.py
@@ -0,0 +1,11 @@
+"""
+find the runner up score
+"""
+
+
+def return_runner_up_score(number, array):
+    return number, array
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
